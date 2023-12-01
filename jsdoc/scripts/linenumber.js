39225f0d4c810030f12196dd35ca32698fa8e06c
Jon Robson <jdlrobson@gmail.com>
2023-11-30 18:05:47 -0800

    Add files via upload

---
+++ jsdoc/scripts/linenumber.js
@@ -0,0 +1,24 @@
+( function () {
+	var source = document.getElementsByClassName( 'prettyprint source linenums' ),
+		i = 0,
+		lineNumber = 0,
+		lineId,
+		lines,
+		totalLines,
+		anchorHash;
+
+	if ( source && source[ 0 ] ) {
+		anchorHash = document.location.hash.slice( 1 );
+		lines = source[ 0 ].getElementsByTagName( 'li' );
+		totalLines = lines.length;
+
+		for ( ; i < totalLines; i++ ) {
+			lineNumber++;
+			lineId = 'line' + lineNumber;
+			lines[ i ].id = lineId;
+			if ( lineId === anchorHash ) {
+				lines[ i ].className += ' selected';
+			}
+		}
+	}
+}() );
